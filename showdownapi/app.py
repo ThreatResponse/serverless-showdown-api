643ab0835bda0da2c784f146e2e63fcdc43db667
andrewkrug <andrewkrug@gmail.com>
2017-03-04 11:26:58 -0800

    updates for s3 writes only

--- showdownapi/app.py
+++ showdownapi/app.py
@@ -1,13 +1,28 @@
+import os
+import boto3
+import uuid
 from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class EventToElastic():
-    pass
+s3_bucket = os.environ["S3_BUCKET"]
+
 
 class EventToDisk():
+    """Writes the content to s3 for ingestion to elastic later."""
+    def __init__(self):
+        self.client = boto3.client('s3')
+
+    def to_s3(self, contents):
+        """Puts contents of a post to s3."""
+        response = self.client.put_object(
+            ACL='private',
+            Key="{id}.json".format(id=uuid.uuid4().hex),
+            Body=str(contents),
+            Bucket=s3_bucket
+        )
     pass
 
 
@@ -15,6 +30,8 @@ class ServerlessProfile(Resource):
     def post(self):
         try:
             data = request.json
+            e = EventToDisk()
+            e.to_s3(data)
             return {'result': 'stored'}, 200
         except:
             return {'result': 'could not process payload'}, 500
